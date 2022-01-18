591fb039a4fd213b540272bade582264ffd95cf5
Grzegorz Cichosz <grzes.cichosz@gmail.com>
2022-01-18 15:08:42 +0100

    add comments and stuff

--- tri_list.h
+++ tri_list.h
@@ -13,59 +13,76 @@
 
 #include "tri_list_concepts.h"
 
-// moze overkill?
+// moze overkill? czy zamiast
+// template< typename T>
+// wewn klasy tri_list robić
+// template <my_type<T1, T2, T3> T>?
 template <typename T, typename T1, typename T2, typename T3>
 concept my_type = std::same_as<T, T1> || std::same_as<T, T2>
                   || std::same_as<T, T3>;
 
 
+// Compose two modifiers into a single one. Works just like standard function
+// composition in maths ie. if the result of compose(f2, f1) would be applied
+// on some x then it would evaluate to f2(f1(x)).
 template <typename T, modifier<T> F1, modifier<T> F2>
 inline auto compose(F2 f2, F1 f1)
 {
   return [f1, f2] (T e) { return f2(f1(e)); };
 }
 
+
+// Identity function for any type.
 template <typename T>
 inline std::function<T(T)> identity()
 {
   return [] (T e) { return e; };
 }
 
+// A a collection that may store elements of 3 types: T1, T2 and T3.
 template <typename T1, typename T2, typename T3>
-class tri_list
-{
+class tri_list {
+  // An alias for the variants stored by the list.
   using var_t = std::variant<T1, T2, T3>;
 
+  // Alias for the basic modifier for a given type.
   template <typename T>
   using mod_type = std::function<T(T)>;
-  
+
+  // Contents will be store in a vector of variants.
   std::vector<var_t> contents;
 
+  // Modifier for each type is stored in this tuple.
   std::tuple<mod_type<T1>, mod_type<T2>, mod_type<T3>>
   mods = {
     identity<T1>(), identity<T2>(), identity<T3>()
   };
 
+  // Accessing the type T's current modifier. It returns a reference so that
+  // the result can be further modified.
   template <typename T>
   mod_type<T>& get_mod()
   {
     return std::get<mod_type<T>>(mods);
   }
-  
+
 public:
+  // Constructor for an empty list.
   tri_list() : contents() {}
 
+  // Make a list out of given arguments in a initialiser list (all may have
+  // different types).
   tri_list(std::initializer_list<var_t> init) : contents(init) {}
 
-  template <my_type<T1, T2, T3> T>
-  // template <typename T>
+  // Add a new element of type T.
+  template <typename T>
   void push_back(const T& t)
   {
     contents.push_back(var_t(t));
   }
 
-  template <my_type<T1, T2, T3> T>
-  // template <typename T>
+  // Get a view of all of the elements of type T stored in the list.
+  template <typename T>
   auto range_over()
   {
     auto filter = [] <typename E> (E) {
@@ -77,13 +94,15 @@ public:
     };
 
     auto mod = get_mod<T>();
-    
-    return contents | std::views::filter(visit_filter)
+
+    return contents
+      | std::views::filter(visit_filter)
       | std::views::transform([mod] (const var_t & v) {
         return mod(std::get<T>(v));
-    });
+      });
   }
 
+  // Modify all elements of type T with a modifier m.
   template <typename T, modifier<T> F>
   void modify_only(F m = F{})
   {
@@ -91,11 +110,18 @@ public:
     mod = compose<T>(m, mod);
   }
 
+  // Undo all modifications that were done on elements of type T.
   template <typename T>
   void reset()
   {
     get_mod<T>() = identity<T>();
   }
+
+private:
+  // An iterator, necessary for the begin() and end() methods.
+  struct tri_iterator {
+
+  };
 };
 
 #endif  // _TRI_LIST_H_
